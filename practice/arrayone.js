6db00df9db98d9c2471f07880b458ca300ea1684
sainath711 <sainathsaii007@gmail.com>
2024-06-07 11:25:36 +0530

    js

---
+++ practice/arrayone.js
@@ -0,0 +1,4 @@
+let cars=['benj','bmw','kia','honda']
+for(let i=0;i<cars.length;i++){
+    console.log(cars[i])
+}
---
+++ practice/arraytwo.js
@@ -0,0 +1,4 @@
+let s=[10,20,30,40,50]
+for(let i=0;i<s.length;i++){
+    console.log(s[i])
+}
