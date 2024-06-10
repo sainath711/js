a4ea998eb91bcfeb002544325ac99d23d4f4df60
sainath711 <sainathsaii007@gmail.com>
2024-06-10 19:12:39 +0530

    js

--- practice/object/obj1.js
+++ practice/object/obj1.js
@@ -9,18 +9,3 @@ document.write(jsonString);
 console.log(obj);
 
 
-// // Wait for the DOM to be fully loaded
-// document.addEventListener('DOMContentLoaded', (event) => {
-//      // Define an array
-//      var obj = [101, 102, 103];
- 
-//      // Convert the array to a JSON string
-//      var jsonString = JSON.stringify(obj);
- 
-//      // Write the JSON string to the document
-//      document.write(jsonString);
- 
-//      // Log the original array to the console
-//      console.log(obj);
-//  });
- 
