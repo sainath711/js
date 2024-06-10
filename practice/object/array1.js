021a6dcfd96dda59bc2c964da1498a3ba7334d16
sainath711 <sainathsaii007@gmail.com>
2024-06-10 19:03:16 +0530

    js

--- practice/object/array1.js
+++ practice/object/array1.js
@@ -1,4 +1,8 @@
+//Array , array length ,boolean values
+
 var arr=[101,102,103,104]
+
+//to print the given output
 console.log(arr)
 console.log(arr.length)
 console.log(arr.length>3)
--- practice/object/obj1.js
+++ practice/object/obj1.js
@@ -1,10 +1,26 @@
-var obj={
-     eid:101,
-     ename:'sainath',
-     eage:23
+var obj=[101,102,103,104]
 
-}
+//converting array into stringfy
+var jsonString=JSON.stringify(obj);
+
+//converted stringfy into document.write
+document.write(jsonString);
+
 console.log(obj);
 
-JSON.stringify(obj)
-console.log(obj.length);
+
+// // Wait for the DOM to be fully loaded
+// document.addEventListener('DOMContentLoaded', (event) => {
+//      // Define an array
+//      var obj = [101, 102, 103];
+ 
+//      // Convert the array to a JSON string
+//      var jsonString = JSON.stringify(obj);
+ 
+//      // Write the JSON string to the document
+//      document.write(jsonString);
+ 
+//      // Log the original array to the console
+//      console.log(obj);
+//  });
+ 
