e26e454f6cf6bce1c2b7a0cf3ba921644ed8c789
sainath711 <sainathsaii007@gmail.com>
2024-06-12 09:32:37 +0530

    js-Events

---
+++ DOM1/DOMImage/data.js
@@ -0,0 +1,16 @@
+function Bigimage(x){
+    x.style.height="500px";
+    x.style.width="600px";
+
+}
+function NormalImage(x){
+    x.style.height="250px";
+    x.style.width="275px"
+}
+
+
+
+
+
+
+
