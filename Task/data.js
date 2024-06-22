91f7016f9208b583fe23aa39a3961f6761a44ce5
sainath711 <sainathsaii007@gmail.com>
2024-06-22 11:39:53 +0530

    Task

---
+++ Task/data.js
@@ -0,0 +1,20 @@
+let employees=[
+    {id:101,name:'Rahul',sal:45000},
+    {id:102,name:'Sonia',sal:55000},
+    {id:103,name:'Priyanka',sal:65000},
+    {id:104,name:'Modi',sal:75000},
+]
+function displayData(){
+    let rows='';
+    for(emp of employees){
+        rows=rows+`<tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.sal}</td>
+        </tr>`
+    }
+    document.getElementsByTagName('tbody')[0].innerHTML=rows
+}
+
+
+
